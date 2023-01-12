a77a6af68e57614eacdf355ec0140752d57810c2
Avinash Krishnamoorthy <saiavinashkrishnamoorthy@gmail.com>
2023-01-12 09:42:52 +0530

    Day03 Program02

---
+++ src/day03/Program02.java
@@ -0,0 +1,30 @@
+package day03;
+
+public class Program02 {
+
+	public static void main(String[] args) {
+		// >, <, >=, <=, ==, !=,
+		//true / false
+		
+		
+		int x=10;
+		int y=20;
+		double z=10.0;
+		double a=10.2;
+		
+		System.out.println(x>y);
+		System.out.println(y>x);
+		System.out.println(y>=y);
+		
+		System.out.println(x<y);
+		System.out.println(x==z);
+		System.out.println(x==a);
+		System.out.println(x!=y);
+		System.out.println(x!=z);
+		
+		
+		
+
+	}
+
+}
