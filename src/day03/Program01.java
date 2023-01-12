5f613b197da6f5e0273f41e66c05bfbeacdec533
Avinash Krishnamoorthy <saiavinashkrishnamoorthy@gmail.com>
2023-01-12 09:22:31 +0530

    Day-03 Program01

---
+++ src/day03/Program01.java
@@ -0,0 +1,32 @@
+package day03;
+
+public class Program01 {
+
+	public static void main(String[] args) {
+		//+ - * / %
+		
+		//int (op) int---->int
+		//double (op) int--->double
+		
+		int a=10;
+		int b=2;
+		double c=5.5;
+		System.out.println("Add");
+		System.out.println(a+b);
+		System.out.println(a+c);
+		System.out.println("sub");
+		System.out.println(a-b);
+		System.out.println(a-c);
+		System.out.println("Mul");
+		System.out.println(a*b);
+		System.out.println(a*c);
+		System.out.println("#############");
+		System.out.println(10/3);
+		System.out.println(10/3.0);
+		System.out.println("#############");
+		System.out.println(10%3);
+		System.out.println(10%3.0);
+		
+	}
+
+}
