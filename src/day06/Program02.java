c71dc3659797cfdbfa52378cd084caff31f6dc73
Avinash Krishnamoorthy <saiavinashkrishnamoorthy@gmail.com>
2023-01-19 09:39:18 +0530

    Day06- IfElse program

---
+++ src/day06/Program02.java
@@ -0,0 +1,21 @@
+package day06;
+
+public class Program02 {
+
+	public static void main(String[] args) {
+		int a=100;
+		int b=20;
+		
+		if(a>b)
+		{
+			System.out.println("a is greater");
+		}
+		else
+		{
+			System.out.println("b is greater");
+		}
+		
+
+	}
+
+}
---
+++ src/day06/Program03.java
@@ -0,0 +1,19 @@
+package day06;
+
+public class Program03 {
+
+	public static void main(String[] args) {
+		int x=101;
+		
+		if(x%2==0)
+		{
+			System.out.println("even");
+		}
+		else
+		{
+			System.out.println("odd");
+		}
+
+	}
+
+}
