7bd119133fbfb36d01ce439e96c4b1159a095acf
Avinash Krishnamoorthy <saiavinashkrishnamoorthy@gmail.com>
2023-01-19 09:28:08 +0530

    Day06-If statement

---
+++ src/day06/Program01.java
@@ -0,0 +1,19 @@
+package day06;
+
+public class Program01 {
+
+	public static void main(String[] args) {
+		
+		int a=100;
+		int b=200;
+		if(a>b)
+		{
+			System.out.println("Good Day..!");
+			System.out.println("Good Morning..!");
+		}
+		
+		System.out.println("##############");
+
+	}
+
+}
