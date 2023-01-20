61d6b5e5690d393114a505bef8ab852df353087d
Avinash Krishnamoorthy <saiavinashkrishnamoorthy@gmail.com>
2023-01-20 10:05:55 +0530

    Day07 while&Switch

---
+++ src/day07/Program01.java
@@ -0,0 +1,38 @@
+package day07;
+
+public class Program01 {
+
+	public static void main(String[] args) {
+		
+		String x="WEN";
+		
+		switch (x) {
+		case "SUN":
+			System.out.println("Day-1");
+			break;
+		case "MON":
+			System.out.println("Day-2");
+			break;
+		case "TUE":
+			System.out.println("Day-3");
+			break;
+		case "WEN":
+			System.out.println("Day-4");
+			break;
+		case "THU":
+			System.out.println("Day-5");
+			break;
+		case "FRI":
+			System.out.println("Day-6");
+			break;
+		case "SAT":
+			System.out.println("Day-7");
+			break;
+		default:
+			System.out.println("Invalid Selection");
+			break;
+		}
+
+	}
+
+}
---
+++ src/day07/Program02.java
@@ -0,0 +1,31 @@
+package day07;
+
+public class Program02 {
+
+	public static void main(String[] args) {
+		
+		int x=1;		
+		while(x<=5)//x=1 x=2 x=3 x=4 x=5
+		{
+			System.out.println("Good Day..!");//1 2 3 4 5
+			x=x+1;//x=2 x=3 x=4 x=5 x=6
+		}
+		
+		int y=-5;
+		while(y<=-1)
+		{
+			System.out.println("Good Morning..!");
+			y=y+1;
+		}
+		
+		double a=0.1;
+		while(a<=0.5)
+		{
+			System.out.println("Great Day..!");
+			a=a+0.1;
+		}
+		
+		
+	}
+
+}
